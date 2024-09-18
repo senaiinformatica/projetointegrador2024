adc231b6be0181683543692db5faf4af5f1237e9
professorThiago <thiagomecatronicasenai@gmail.com>
2024-09-18 15:41:53 -0300

    ultimo commit

--- include/senhas.h
+++ include/senhas.h
@@ -2,5 +2,5 @@
 const char *ssid = "MQTT";
 const char *password = "info@134";
 // Credenciais de acesso ao Broker MQTT
-const char *mqtt_server = "172.16.57.45";   // IP do Broker MQTT
+const char *mqtt_server = "test.mosquitto.org";   // IP do Broker MQTT
 const int mqtt_port = 1883;                 // Porta do Broker MQTT
